952b3185d1d6c27f5309d85fb706feba59bfe2b9
Maxim190 <vbrc1212@yandex.ru>
2020-09-27 17:30:01 +0300

    initial commit

---
+++ src/main/java/com/example/groupeditor/GroupEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupEditorApplication.class, args);
+	}
+
+}
